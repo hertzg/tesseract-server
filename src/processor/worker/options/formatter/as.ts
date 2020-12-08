e5f83561c4cd3b8c5078d880e91b0fc0f1ac4334
George Hertz <georgedot@gmail.com>
2020-12-08 01:03:06 +0100

    fix: sort config args to allow pool reuse

--- src/processor/worker/options/formatter/as.ts
+++ src/processor/worker/options/formatter/as.ts
@@ -6,4 +6,6 @@ export const asArguments = <TMap extends { [key: string]: string }>(
   name: string,
   convert: (key: string, value: string) => string,
 ) => (map: TMap): string[] =>
-  Object.entries(map).flatMap(([key, value]) => [name, convert(key, value)]);
+  Object.entries(map)
+    .sort(([a], [b]) => a.localeCompare(b))
+    .flatMap(([key, value]) => [name, convert(key, value)]);
