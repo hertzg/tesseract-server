13f098dd616b7c3ae33a0d5137b5db454df90f4c
George Hertz <georgedot@gmail.com>
2020-12-08 20:30:27 +0100

    fix: disable fancy argument parsing

--- src/argv.ts
+++ src/argv.ts
@@ -1,41 +1,43 @@
 import Yargs from 'yargs';
 import { tmpdir } from 'os';
 
-export default Yargs(process.argv).options({
-  'pool.default.min': {
-    default: 0,
-    type: 'number',
-  },
-  'pool.default.max': {
-    default: 2,
-    type: 'number',
-  },
-  'pool.default.idleTimeoutMillis': {
-    default: 5000,
-    type: 'number',
-  },
-  'pool.default.evictionRunIntervalMillis': {
-    default: 5000,
-    type: 'number',
-  },
-  'http.upload.tmpDir': {
-    default: tmpdir(),
-    type: 'string',
-  },
-  'http.endpoint.status.enable': {
-    default: true,
-    type: 'boolean',
-  },
-  'http.output.jsonSpaces': {
-    default: 0,
-    type: 'number',
-  },
-  'http.input.optionsField': {
-    default: 'options',
-    type: 'string',
-  },
-  'http.input.fileField': {
-    default: 'file',
-    type: 'string',
-  },
-}).argv;
+export default Yargs(process.argv)
+  .parserConfiguration({ 'dot-notation': false, 'camel-case-expansion': false })
+  .options({
+    'pool.default.min': {
+      default: 0,
+      type: 'number',
+    },
+    'pool.default.max': {
+      default: 2,
+      type: 'number',
+    },
+    'pool.default.idleTimeoutMillis': {
+      default: 5000,
+      type: 'number',
+    },
+    'pool.default.evictionRunIntervalMillis': {
+      default: 5000,
+      type: 'number',
+    },
+    'http.upload.tmpDir': {
+      default: tmpdir(),
+      type: 'string',
+    },
+    'http.endpoint.status.enable': {
+      default: true,
+      type: 'boolean',
+    },
+    'http.output.jsonSpaces': {
+      default: 0,
+      type: 'number',
+    },
+    'http.input.optionsField': {
+      default: 'options',
+      type: 'string',
+    },
+    'http.input.fileField': {
+      default: 'file',
+      type: 'string',
+    },
+  }).argv;
