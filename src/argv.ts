08f7094eadd24a1601a9241cce6023816c729710
George Hertz <georgedot@gmail.com>
2020-12-20 15:26:26 +0100

    feat: document all options and improve `--help`

--- src/argv.ts
+++ src/argv.ts
@@ -1,47 +1,84 @@
-import Yargs from 'yargs';
+import Yargs, { terminalWidth } from 'yargs';
 import { tmpdir } from 'os';
 
 export default Yargs(process.argv)
   .parserConfiguration({ 'dot-notation': false, 'camel-case-expansion': false })
+  .wrap(terminalWidth())
+  .scriptName('tesseract-server')
+  .usage(
+    [
+      '$0 [options]',
+      '',
+      'A small lightweight http server exposing tesseract as a service.',
+    ].join('\n'),
+  )
   .options({
     'pool.default.min': {
+      description: 'Minimum number of processes to keep waiting in each pool',
       default: 0,
       type: 'number',
     },
     'pool.default.max': {
+      description:
+        'Maximum number of processes to spawn for each pool after which requests are queued',
       default: 2,
       type: 'number',
     },
     'pool.default.idleTimeoutMillis': {
+      description:
+        'Time (in milliseconds) a processes can stay idle in queue before eviction',
       default: 5000,
       type: 'number',
     },
     'pool.default.evictionRunIntervalMillis': {
+      description: 'Time interval (in milliseconds) between eviction checks',
       default: 5000,
       type: 'number',
     },
     'http.upload.tmpDir': {
+      description: 'Path to where temp uploads are saved to',
       default: tmpdir(),
       type: 'string',
     },
     'http.endpoint.status.enable': {
+      description: 'Enable /status endpoint',
       default: true,
       type: 'boolean',
     },
     'http.endpoint.health.enable': {
+      description: 'Enable /.well-known/health/* endpoints and health checkers',
       default: true,
       type: 'boolean',
     },
-    'http.output.jsonSpaces': {
-      default: 0,
-      type: 'number',
-    },
     'http.input.optionsField': {
+      description: 'Multipart field name containing OCR Options',
       default: 'options',
       type: 'string',
     },
     'http.input.fileField': {
+      description: 'Multipart field name containing OCR file',
       default: 'file',
       type: 'string',
     },
-  }).argv;
+    'http.output.jsonSpaces': {
+      description:
+        'Enable json pretty printing and set number of spaces to use for indentation',
+      default: 0,
+      type: 'number',
+    },
+  })
+  .example([
+    ['$0 --http.output.jsonSpaces 2', 'Enable JSON pretty printing'],
+    [
+      '$0 --http.endpoint.status.enable false --http.endpoint.health.enable false',
+      'Disable Status and Health endpoints',
+    ],
+  ])
+  .epilogue(
+    [
+      'References:',
+      '  GitHub: https://github.com/hertzg/tesseract-server',
+      '  Discussions: https://github.com/hertzg/tesseract-server/discussions',
+      '  Issues: https://github.com/hertzg/tesseract-server/issues',
+    ].join('\n'),
+  ).argv;
